20d358a3e44cacf2b6d85942fd23c257718ef303
Yevhen Kontselenko <105706808+YevhenKon@users.noreply.github.com>
2023-03-20 00:15:16 +0200

    2 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,8 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-// console.log(galleryItems);
 
 const makeGalleryItems = galleryItem => {
     const { original, preview, description } = galleryItem;
-    // console.log(galleryItem);
+    
     return `<li class="gallery__item">
   <a class="gallery__link" href=${original}>
     <img
@@ -23,7 +20,6 @@ const makeGalleryPictures = galleryItems.map(makeGalleryItems).join('')
 const galleryElements = document.querySelector('.gallery')
 
 galleryElements.insertAdjacentHTML('afterbegin', makeGalleryPictures)
-// console.log(makeGalleryPictures);
 
 galleryElements.addEventListener('click', (evt) => {
     evt.preventDefault()
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-// console.log(galleryItems);
+const makeGalleryItems = galleryItem => {
+    const { original, preview, description } = galleryItem;
+    
+    return `<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} />
+   </a>
+</li>`
+}
+
+const makeGalleryPictures = galleryItems.map(makeGalleryItems).join('')
+
+const galleryElements = document.querySelector('.gallery')
+
+galleryElements.insertAdjacentHTML('afterbegin', makeGalleryPictures)
+
+galleryElements.addEventListener('click', (evt) => {
+    evt.preventDefault()
+
+    if (evt.target.tagName !== 'IMG') {
+        return
+    }
+    
+    const lightbox = new SimpleLightbox('.gallery a',
+        {
+            caption: 'alt',
+            captionDelay: 250
+        });
+
+})
+
+
+
